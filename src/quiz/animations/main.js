b2bc118ff752a9802b36bda56ac0db070f6e5676
sankar <rsankar19981@gmail.com>
2024-03-06 22:26:30 -0500

    Inclass Activity 022924

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -1,19 +1,19 @@
 const aliceTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
+];
+
+const aliceTiming = {
     duration: 2000,
     iterations: 1,
     fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
+}
 
-  alice1
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+alice1
     .animate(aliceTumbling, aliceTiming)
     .finished
     .then((res) => {
@@ -27,16 +27,24 @@ const aliceTumbling = [
             })
     });
 
-  // Promise chain  
-  // alice1.animate(aliceTumbling, aliceTiming).finished  
-  //   .then(() => {
-  //       return alice2
-  //               .animate(aliceTumbling, aliceTiming)
-  //               .finished;     
-  //   })
-  //   .then(() => {
-  //     return alice3
-  //             .animate(aliceTumbling, aliceTiming)
-  //             .finished;
-  //   })
-  //   .catch((err) => alert(`Error when promising ... ${err.message}`));
+// Promise chain
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => {
+//       return alice2
+//               .animate(aliceTumbling, aliceTiming)
+//               .finished;
+//   })
+//   .then(() => {
+//     return alice3
+//             .animate(aliceTumbling, aliceTiming)
+//             .finished;
+//   })
+//   .catch((err) => alert(`Error when promising ... ${err.message}`));
+
+async function animateAlice() {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    await alice3.animate(aliceTumbling, aliceTiming).finished;
+}
+
+animateAlice()
